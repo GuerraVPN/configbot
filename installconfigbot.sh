f326ef1a979e53863cc892e5f7db7f3216800da1
GuerraVPN <81371258+GuerraVPN@users.noreply.github.com>
2021-10-12 21:15:36 -0300

    Update installconfigbot.sh

--- installconfigbot.sh
+++ installconfigbot.sh
@@ -1,17 +1,19 @@
 clear
 echo -e "\E[41;1;37m INSTALANDO. \E[0m"
-sleep 3
+sleep 2
 apt install git
 clear
 echo -e "\E[41;1;37m INSTALANDO.. \E[0m"
-sleep 3
+sleep 2
 git clone https://github.com/GuerraVPN/configbot
 clear
 echo -e "\E[41;1;37m INSTALANDO... \E[0m"
-sleep 3
+sleep 2
 rm installconfigbot.sh /root/configbot/installconfigbot.sh
 clear
 mv configbot bot
+clear
+chmod +x /root/bot/configbot.sh
 clear
 echo-e "\E[41;1;37m ...INICIANDO... \E[0m"
 bash /root/bot/configbot.sh
